65da16489e5a288b2f2735ac51ba17b681b0bf5b
Ehab <okashahehab@gmail.com>
2025-09-20 15:12:34 +0300

    Add Create View with Validation

---
+++ ASP.NET_Assignment/DTOs/CreateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET.Assignment.PL.DTOs
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code Is Required!")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name Is Required!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Date Of Creation Is Required!")]
+        public DateTime DateOfCreation { get; set; }
+    }
+}
