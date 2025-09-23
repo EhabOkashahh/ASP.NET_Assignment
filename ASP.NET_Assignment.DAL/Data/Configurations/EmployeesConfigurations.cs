7762587adf6ab1027583c10245725d112051e930
Ehab <okashahehab@gmail.com>
2025-09-23 11:32:38 +0300

    Adding EmployeeModel and Configurations and add the Migrations

---
+++ ASP.NET_Assignment.DAL/Data/Configurations/EmployeesConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using ASP.NET_Assignment.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Assignment.DAL.Data.Configurations
+{
+    internal class EmployeesConfigurations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18,2)");
+            builder.Property(E=>E.Id).UseIdentityColumn(10,10);
+        }
+    }
+}
