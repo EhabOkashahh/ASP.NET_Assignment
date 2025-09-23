9f90a949c16210853e3019a6e78410908f2b3f52
Ehab <okashahehab@gmail.com>
2025-09-23 12:17:05 +0300

    update

---
+++ ASP.NET_Assignment/Controllers/EmployeesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.Assignment.PL.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
